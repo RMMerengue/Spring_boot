c1fb440d12c6e6f7d97276bbfe8945e01737ea29
RMMerengue <1017703403@qq.com>
2022-07-15 19:39:13 +0800

    yml配置

---
+++ springboot_03_yaml/src/main/java/com/itheima/controller/BookController.java
@@ -0,0 +1,33 @@
+package com.itheima.controller;
+
+import com.itheima.MyDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Rest模式
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Value("${country}")
+    private String country1;
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private MyDataSource myDataSource;
+
+    @GetMapping
+    public String getById(){
+        System.out.println("springboot is running...");
+        System.out.println("coutry1---->" + country1);
+        System.out.println("coutry1---->" + env.getProperty("country"));
+        System.out.println(myDataSource);
+        return "springboot is running...";
+    }
+}
