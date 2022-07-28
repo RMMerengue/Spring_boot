41b6a90e1534e37836fb5fa2589c50534dbeaa8a
RMMerengue <1017703403@qq.com>
2022-07-28 18:44:51 +0800

    热部署

---
+++ springboot_12_hot_deploy/src/test/java/com/itheima/Service/BookServiceTestCase.java
@@ -0,0 +1,63 @@
+package com.itheima.Service;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.domain.Book;
+import com.itheima.service.IBookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookServiceTestCase {
+
+    @Autowired
+    private IBookService bookService;
+
+    @Test
+    void testById(){
+        System.out.println(bookService.getById(4));
+    }
+
+    @Test
+    void testSave(){
+        Book book = new Book();
+        book.setType("test date 123");
+        book.setName("test data 123");
+        book.setDescription("test data 123");
+        bookService.save(book);
+    }
+
+    @Test
+    void testUpdate(){
+        Book book = new Book();
+        book.setId(13);
+        book.setType("test date 321");
+        book.setName("test data 321");
+        book.setDescription("test data 321");
+        bookService.updateById(book);
+    }
+
+    @Test
+    void testDelete(){
+        bookService.removeById(13);
+    }
+
+    @Test
+    void testGetAll(){
+//        bookService.list();
+    }
+
+    @Test
+    void testGetPage(){
+        IPage<Book> page = new Page<Book>(2,5);
+        bookService.page(page);
+        System.out.println(page.getCurrent());
+        System.out.println(page.getSize());
+        System.out.println(page.getTotal());
+        System.out.println(page.getPages());
+        System.out.println(page.getRecords());
+    }
+
+
+}
