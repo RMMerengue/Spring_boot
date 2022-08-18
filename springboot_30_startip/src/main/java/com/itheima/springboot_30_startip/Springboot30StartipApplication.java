98797f36012aa2bf05f5e107e752ab49c1e81639
RMMerengue <1017703403@qq.com>
2022-08-18 22:34:02 +0800

    springboot启动流程

---
+++ springboot_30_startip/src/main/java/com/itheima/springboot_30_startip/Springboot30StartipApplication.java
@@ -0,0 +1,13 @@
+package com.itheima.springboot_30_startip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot30StartipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot30StartipApplication.class, args);
+    }
+
+}
---
+++ springboot_30_startip/src/test/java/com/itheima/springboot_30_startip/Springboot30StartipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springboot_30_startip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot30StartipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
